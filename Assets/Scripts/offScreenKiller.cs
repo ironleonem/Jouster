0deb787e87800d0ec045ec3b9f6392941990c095
Michael Pattie <mjpattie@gmail.com>
2016-07-23 16:47:02 -0700

    Added level kill function.

--- Assets/Scripts/offScreenKiller.cs
+++ Assets/Scripts/offScreenKiller.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class offScreenKiller : MonoBehaviour {
 
+	Transform endTrans;
+	Transform gameManager;
+
 	// Use this for initialization
 	void Start () {
-	
+		gameManager = GameObject.FindObjectOfType<GameManagerScript> ().transform;
+		endTrans = transform.GetChild (0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(endTrans.position.x < gameManager.position.x - 10){
+			Destroy(this.gameObject);
+		}
 	}
 }
